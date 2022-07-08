e41e2dc8afeba5af9bbd3bbec7c33bde9b4f8839
manuelsansoresg <manuelsansoresg@gmail.com>
2022-07-08 17:13:26 +0000

    install laravel

---
+++ app/Http/Controllers/DeployController.php
@@ -0,0 +1,16 @@
+<?php
+
+namespace App\Http\Controllers;
+
+use Illuminate\Http\Request;
+
+class DeployController extends Controller
+{
+    public function index()
+    {
+
+        $output = shell_exec("git pull 2>&1");
+        echo '<pre>';
+        echo $output;
+    }
+}
