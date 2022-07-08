1df9c79744bffd38f75737535199638b66d79633
manuelsansores <manuelsansoresg@gmail.com>
2022-07-08 12:21:22 -0500

    web

--- routes/web.php
+++ routes/web.php
@@ -21,7 +21,6 @@ Route::get('/', function () {
 Route::get('/data/web', function () {
     return view('welcome');
 });
- 
 
 Route::any('/deploy/index', [DeployController::class, 'index']);
 Route::post('/deploy/index', [DeployController::class, 'index']);
