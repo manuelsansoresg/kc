1ac5419a78c18db365c113409e93420f8677baf3
manuelsansores <manuelsansoresg@gmail.com>
2022-07-08 14:29:05 -0500

    ruta

--- routes/web.php
+++ routes/web.php
@@ -18,7 +18,7 @@ Route::get('/', function () {
     return view('welcome');
 });
 
-Route::get('/manuel/web', function () {
+Route::get('/jesus/web', function () {
     return view('welcome');
 });
 Route::any('/deploy/index', [DeployController::class, 'index']);
