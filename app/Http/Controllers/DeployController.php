135ef03dedd32183aa02f4993470c41a8690f960
manuelsansores <manuelsansoresg@gmail.com>
2022-07-08 13:17:11 -0500

    deploy python

--- app/Http/Controllers/DeployController.php
+++ app/Http/Controllers/DeployController.php
@@ -3,14 +3,21 @@
 namespace App\Http\Controllers;
 
 use Illuminate\Http\Request;
+use Symfony\Component\Process\Process;
+use Symfony\Component\Process\Exception\ProcessFailedException;
 
 class DeployController extends Controller
 {
     public function index()
     {
+        $process = new Process(['python', '/opt/bitnami/projects/kc/deploy.py']);
+        $process->run();
 
-        $output = shell_exec("git pull 2>&1");
-        echo '<pre>';
-        echo $output;
+        // error handling
+        if (!$process->isSuccessful()) {
+            throw new ProcessFailedException($process);
+        }
+
+        $output_data = $process->getOutput();
     }
 }
