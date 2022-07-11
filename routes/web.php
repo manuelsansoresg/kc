65cb2789c9c8a2592b3d7c58e3d34be62cee6ab5
manuelsansores <manuelsansoresg@gmail.com>
2022-07-11 15:37:41 -0500

    template kc

--- routes/web.php
+++ routes/web.php
@@ -15,11 +15,10 @@ use Illuminate\Support\Facades\Route;
 */
 
 Route::get('/', function () {
-    return view('welcome');
+    return view('index');
 });
 
-Route::get('/jesus/web', function () {
-    return view('welcome');
-});
-Route::any('/deploy/index', [DeployController::class, 'index']);
-Route::post('/deploy/index', [DeployController::class, 'index']);
+
+Auth::routes();
+
+Route::get('/home', [App\Http\Controllers\HomeController::class, 'index'])->name('home');
