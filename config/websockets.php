2799507fefb7c5fb3c90dfbc4679487ca2aa318f
manuelsansores <manuelsansoresg@gmail.com>
2022-08-05 14:29:32 -0500

    ssl

--- config/websockets.php
+++ config/websockets.php
@@ -116,13 +116,13 @@ return [
          * certificate chain of issuers. The private key also may be contained
          * in a separate file specified by local_pk.
          */
-        'local_cert' => env('LARAVEL_WEBSOCKETS_SSL_LOCAL_CERT', null),
+        'local_cert' => env('LARAVEL_WEBSOCKETS_SSL_LOCAL_CERT', '/opt/bitnami/apache2/conf/bitnami/certs/kc/server.crt'),
 
         /*
          * Path to local private key file on filesystem in case of separate files for
          * certificate (local_cert) and private key.
          */
-        'local_pk' => env('LARAVEL_WEBSOCKETS_SSL_LOCAL_PK', null),
+        'local_pk' => env('LARAVEL_WEBSOCKETS_SSL_LOCAL_PK', '/opt/bitnami/apache2/conf/bitnami/certs/kc/server.key'),
 
         /*
          * Passphrase for your local_cert file.
