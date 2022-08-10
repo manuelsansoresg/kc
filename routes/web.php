c8ae7d64c6ee588f92e4a6abe05ac6b5fb444381
manuelsansores <manuelsansoresg@gmail.com>
2022-08-10 10:32:30 -0500

    politicas

--- routes/web.php
+++ routes/web.php
@@ -25,7 +25,7 @@ Route::get('/', function () {
 Auth::routes();
 
 
-Route::get('/politicas-de-privacidad', function () {
+Route::get('/politicas', function () {
     return view('politicas');
 });
 
