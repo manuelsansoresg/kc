a347160563fbb5fea365e91f740b2a5adea92aca
manuelsansores <manuelsansoresg@gmail.com>
2022-08-24 21:33:46 -0500

    down product

--- resources/views/panel/module/checkup/product.blade.php
+++ resources/views/panel/module/checkup/product.blade.php
@@ -7,7 +7,7 @@
     </h6>
 </div> --}}
 <a href="#" class="project-title">
-    <div class="preview-icon-wrap text-gray mt-n1"><em class="ni ni-sign-mxn-alt"></em></div>
+    <div class="preview-icon-wrap text-gray mt-1"><em class="ni ni-sign-mxn-alt"></em></div>
     <div class="project-info">
         <h6 class="title"> &nbsp; {{ $alias_product }}</h6>
     </div>
