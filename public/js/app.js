8985984f2ff9fbaffd27a6bd8d002798aab37959
manuelsansores <manuelsansoresg@gmail.com>
2022-09-26 11:32:21 -0500

    order

--- public/js/app.js
+++ public/js/app.js
@@ -678,7 +678,8 @@ document.addEventListener('DOMContentLoaded', function () {
   var credit_id = $('#credit-profile-credit_id').val();
   var table = NioApp.DataTable('#dt-acctions-profile', {
     processing: true,
-    order: [[5, 'asc']],
+    ordering: true,
+    order: [[5, 'desc']],
     responsive: {
       details: {
         renderer: function renderer(api, rowIdx, columns) {
