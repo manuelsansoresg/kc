56e7dd0665ef10442e9d393785829c0cfb78411f
manuelsansores <manuelsansoresg@gmail.com>
2022-09-27 12:50:56 -0500

    horas

--- app/Helpers/Utilities.php
+++ app/Helpers/Utilities.php
@@ -66,7 +66,7 @@ if (!function_exists('deadlineKc')) {
         $interval = $date_init->diff($date_fin);
         $hour = $interval->format('%H');
         $rest = 0;
-        if ($hour <= $max_hour) {
+        if ($hour < $max_hour) {
             $rest       = $max_hour - $hour;
         }
 
@@ -79,7 +79,7 @@ if (!function_exists('deadlineKc')) {
             $lbl_hour = 'Vencido';
             $color      = 'danger';
         } else {
-            if ($hour > 5) {
+            if ($hour > 18) {
                 $color = ($hour >= $max_hour) ? 'danger' : 'warning';
             }
         }
