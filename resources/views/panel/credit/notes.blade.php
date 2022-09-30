440e517db0eb8ea2a341571d979c5e120d6aac9f
manuelsansores <manuelsansoresg@gmail.com>
2022-09-30 12:00:12 -0500

    Mejoras en las notas del perfil del crédito

--- resources/views/panel/credit/notes.blade.php
+++ resources/views/panel/credit/notes.blade.php
@@ -1,14 +1,17 @@
 @foreach ($notes as $row_note)
     @php
         $get_note = $row_note->note;
+        $user = $get_note->userNote;
     @endphp
     <div class="bq-note">
         <div class="bq-note-item">
             <div class="bq-note-text">
                 <p> {{ $get_note->description }}</p>
             </div>
-            <div class="bq-note-meta">
-                <span class="bq-note-added">Agregado el <span class="date">
+            <div class="bq-note-meta text-end">
+                <span class="bq-note-added">
+                    {{ $user->name}} {{ $user->last_name}}
+                    el <span class="date">
                         {{ formatDateNameMonth($get_note->created_at) }}
                     </span> </span>
 
