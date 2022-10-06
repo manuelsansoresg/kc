89e65595b1b88a6665681afe9eae51f85a68430f
manuelsansores <manuelsansoresg@gmail.com>
2022-10-06 12:36:53 -0500

    toast

--- resources/views/layouts/admin.blade.php
+++ resources/views/layouts/admin.blade.php
@@ -757,6 +757,18 @@
             <!-- wrap @s -->
             <div class="nk-wrap ">
                 <!-- main header @s -->
+                <div aria-live="polite" aria-atomic="true" class="position-relative">
+                    <!-- Position it: -->
+                    <!-- - `.toast-container` for spacing between toasts -->
+                    <!-- - `.position-absolute`, `top-0` & `end-0` to position the toasts in the upper right corner -->
+                    <!-- - `.p-3` to prevent the toasts from sticking to the edge of the container  -->
+                    <div class="toast-container position-absolute top-0 end-0 p-3" style="z-index: 9999">
+                  
+                      <!-- Then put toasts within -->
+                      <div id="content-toast"></div>
+
+                    </div>
+                  </div>
                 <div class="nk-header nk-header-fixed is-light">
                     <div class="container-fluid">
                         <div class="nk-header-wrap">
@@ -903,41 +915,7 @@
                 </div>
                 <!-- main header @e -->
                 <!-- content @s -->
-                <div aria-live="polite" aria-atomic="true" class="position-relative">
-                    <!-- Position it: -->
-                    <!-- - `.toast-container` for spacing between toasts -->
-                    <!-- - `.position-absolute`, `top-0` & `end-0` to position the toasts in the upper right corner -->
-                    <!-- - `.p-3` to prevent the toasts from sticking to the edge of the container  -->
-                    <div class="toast-container position-absolute top-0 end-0 p-3" style="z-index: 9999">
-                  
-                      <!-- Then put toasts within -->
-                      <div id="content-toast"></div>
-                      {{-- <div class="toast" role="alert" aria-live="assertive" aria-atomic="true">
-                        <div class="toast-header bg-primary text-dark">
-                          <img src="..." class="rounded me-2" alt="...">
-                          <strong class="me-auto">Bootstrap</strong>
-                          <small class=" text-dark">just now</small>
-                          <button type="button" class="btn-close" data-bs-dismiss="toast" aria-label="Close"></button>
-                        </div>
-                        <div class="toast-body">
-                          See? Just like this.
-                        </div>
-                      </div> --}}
-                  
-                      {{-- <div class="toast" role="alert" aria-live="assertive" aria-atomic="true">
-                        <div class="toast-header">
-                          <img src="..." class="rounded me-2" alt="...">
-                          <strong class="me-auto">Bootstrap</strong>
-                          <small class="text-muted">2 seconds ago</small>
-                          <button type="button" class="btn-close" data-bs-dismiss="toast" aria-label="Close"></button>
-                        </div>
-                        <div class="toast-body">
-                          Heads up, toasts will stack automatically
-                        </div>
-                      </div> --}}
-
-                    </div>
-                  </div>
+                
                 @yield('content')
 
 
