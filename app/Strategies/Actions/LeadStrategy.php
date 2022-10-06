0737bd1434c9350aac71c701b50fc23dcf754e04
manuelsansores <manuelsansoresg@gmail.com>
2022-10-06 18:25:16 -0500

    nombre asesor

--- app/Strategies/Actions/LeadStrategy.php
+++ app/Strategies/Actions/LeadStrategy.php
@@ -80,7 +80,7 @@ class LeadStrategy implements ActionInterface
             'name' => $name_lead,
             'date_in' => formatDateNameMonth($model_action->start_date),
             'date_fin' => formatDateNameMonth($model_action->end_date),
-            'advisor' => ($advisor!= null)?$advisor->name. $advisor->last_name : '',
+            'advisor' => ($advisor!= null)?$advisor->name. ' '. $advisor->last_name : '',
             'options' => $option
         );
         return $data;
