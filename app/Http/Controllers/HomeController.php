425867848d35cf7984077916ea88c0660b724358
manuelsansores <manuelsansoresg@gmail.com>
2022-11-16 11:55:53 -0600

    json decode

--- app/Http/Controllers/HomeController.php
+++ app/Http/Controllers/HomeController.php
@@ -79,7 +79,7 @@ class HomeController extends Controller
 
     public function sendgrid(Request $request)
     {
-        $data = ($request->all());
+        $data = json_encode($request->all());
         $sendgrid = new Sendgridtest(['body' => $data]);
         $sendgrid->save();
     }
