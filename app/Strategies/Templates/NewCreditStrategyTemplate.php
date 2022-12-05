197657a1199ee847594181e8acc1e271f9626300
manuelsansores <manuelsansoresg@gmail.com>
2022-12-05 11:26:22 -0600

    Error: Deadline en KC-Check up

--- app/Strategies/Templates/NewCreditStrategyTemplate.php
+++ app/Strategies/Templates/NewCreditStrategyTemplate.php
@@ -504,14 +504,8 @@ class NewCreditStrategyTemplate implements TemplateInterface
     {
         $credit     = $history->historyCredit;
         $data_actions = array(
-            HistoryLog::KC_CONTROL_DESK_UPLOAD,
-            HistoryLog::KC_CONTROL_DESK_FORM,
-            HistoryLog::KC_CONTROL_DESK_FORM_STEP_2,
-            HistoryLog::KC_CONTROL_DESK_UPLOAD_3_1,
-            HistoryLog::KC_CONTROL_DESK_FORM_STEP_3_1,
-            HistoryLog::KC_CONTROL_DESK_FORM_STEP_3_2,
-            HistoryLog::KC_CONTROL_DESK_FORM_STEP_4,
-            HistoryLog::KC_CONTROL_DESK_FORM_STEP_5,
+            HistoryLog::KC_CHECK_UP_ACTION_FORM,
+            HistoryLog::KC_CHECK_UP_ACTION_DESITION,
         );
         
         $get_actions = HistoryLog::getByStatus($data_actions, $credit->id);
