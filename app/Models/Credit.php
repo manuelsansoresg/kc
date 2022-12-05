01ca311683c3c3e8c4605fae6c526e10f9ea9f95
manuelsansores <manuelsansoresg@gmail.com>
2022-12-05 11:30:29 -0600

    deadline

--- app/Models/Credit.php
+++ app/Models/Credit.php
@@ -107,7 +107,7 @@ class Credit extends Model
             $max_hour         = 24;
             $data_deadline    = deadlineKc($hour, $max_hour);
             $in_progress      = json_encode((new $templateStrategy)->getPercent($history, true));
-            $dead_line        = json_encode((new $templateStrategy)->moduleDeadline($history));
+            $dead_line        = (new $templateStrategy)->moduleDeadline($history);
             
             $hour             = $data_deadline['lbl_hour'];
             $status_id        = $history->status_id;
