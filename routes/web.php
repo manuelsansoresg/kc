59ec4ff942603b71344d1ce9d0795626ff062f10
manuelsansores <manuelsansoresg@gmail.com>
2023-03-02 17:40:36 -0600

    ruta

--- routes/web.php
+++ routes/web.php
@@ -38,6 +38,7 @@ Route::get('/condiciones', function () {
 
 Route::get('hola', ['\App\Http\Controllers\HomeController', 'surveyHola']);
 
+
 Route::get('reporte/{history_id}', ['\App\Http\Controllers\HomeController', 'report']);
 Route::get('reporte/{history_id}/metodologia', ['\App\Http\Controllers\HomeController', 'method']);
 
