afa89253e481f0265bba57dffbb34b502e701a6f
manuelsansoresg <manuelsansoresg@gmail.com>
2023-04-12 11:50:46 -0600

    lead

--- app/Models/Lead.php
+++ app/Models/Lead.php
@@ -245,11 +245,10 @@ class Lead extends Model
         if ($get_lead == null) {
             $get_lead = new Lead($data_lead);
             $get_lead->save();
-            HistoryLog::move($get_lead->id, HistoryLog::CREATE_PROSPECT, HistoryLog::CREATE_PROSPECT, $request);
+            HistoryLog::move($get_lead->id, HistoryLog::CREATE_PROSPECT, HistoryLog::CREATE_PROSPECT);
         } else {
             $get_lead->fill($data_lead)->update();
         }
-
         if ($number_step == 3) {
             $template   = TemplateValues::STRATEGY['lead'];
             (new $template)->move($get_lead->id);
