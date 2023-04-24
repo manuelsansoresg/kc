45498815278224f491e21b3c91b14e670cc80a32
manuelsansoresg <manuelsansoresg@gmail.com>
2023-04-24 17:09:44 -0600

    parent

--- resources/views/quiz/survey_form.blade.php
+++ resources/views/quiz/survey_form.blade.php
@@ -378,7 +378,9 @@
         window.obtenerCreditId = function() {
             var creditId = document.getElementById("credit_id").value;
             console.log(creditId);
-            window.parent.postMessage({ creditId: creditId }, "https://app.kaaxclub.com");
+            if (window.parent !== window) {
+                window.parent.postMessage({ creditId: creditId }, "https://app.kaaxclub.com");
+            }
         }
     </script>
 
