007a929e585791dcbf67936dccf2f122098e9e3d
manuelsansoresg <manuelsansoresg@gmail.com>
2023-04-28 13:03:03 -0600

    Mejoras en cómo se muestran las respeustas de la API En credit profile

--- resources/views/panel/kyc/msg.blade.php
+++ resources/views/panel/kyc/msg.blade.php
@@ -1,56 +1,49 @@
 <div clas="table-responsive" style="overflow: auto">
     <table class="table">
         @foreach ($data_array as $key =>  $row)
-            <tr>
-              <td> 
-                @if (
+        @php
+            $key = trim($key);
+            $new_key = null;
+            $new_row = null;
+            $i_error = false;
+           try {
+            if (
                   $key != 'datosDocProbatorio'
-                  || $key != 'codigoValidacion'
-                  || $key != 'docProbatorio'
-                  || $key != 'datosDocProbatorio'
-                  || $key != 'estatusCurp'
-                  || $key != 'codigoMensaje'
-                  || $key != 'claveMensaje'
-                  || $key != 'codigoValidacion'
-                  || $key != 'estatus'
-                  || $key != 'cic'
-                  || $key != 'numeroEmision'
-                  || $key != 'distritoFederal'
-                  || $key != 'distritoLocal'
-                  || $key != 'ocr'
-                  || $key != 'anioRegistro'
-                  || $key != 'anioEmision'
-                  || $key != 'tipoPersona'
-                  || $key != 'codigoValidacion'
-                  || $key != 'claveMensaje'
-                  )
-                {{ $key }} 
-                @endif
-              </td>
-              @if (
-                  $key != 'datosDocProbatorio'
-                  || $key != 'codigoValidacion'
-                  || $key != 'docProbatorio'
-                  || $key != 'datosDocProbatorio'
-                  || $key != 'estatusCurp'
-                  || $key != 'codigoMensaje'
-                  || $key != 'claveMensaje'
-                  || $key != 'codigoValidacion'
-                  || $key != 'estatus'
-                  || $key != 'cic'
-                  || $key != 'numeroEmision'
-                  || $key != 'distritoFederal'
-                  || $key != 'distritoLocal'
-                  || $key != 'ocr'
-                  || $key != 'anioRegistro'
-                  || $key != 'anioEmision'
-                  || $key != 'tipoPersona'
-                  || $key != 'codigoValidacion'
-                  || $key != 'claveMensaje'
-                  )
-                <td>  {{ json_encode($row) }} </td>
-              @endif
-            </tr>
+                  && $key != 'codigoValidacion'
+                  && $key != 'docProbatorio'
+                  && $key != 'estatusCurp'
+                  && $key != 'codigoMensaje'
+                  && $key != 'claveMensaje'
+                  && $key != 'estatus'
+                  && $key != 'cic'
+                  && $key != 'numeroEmision'
+                  && $key != 'distritoFederal'
+                  && $key != 'distritoLocal'
+                  && $key != 'ocr'
+                  && $key != 'anioRegistro'
+                  && $key != 'anioEmision'
+                  && $key != 'tipoPersona'
+                  && $key != 'claveMensaje'
+                  ) {
+                    $new_key = $key;
+                    $new_row =  json_encode($row) ;
+                  }
+           } catch (\Exception $e) {
+            $i_error = true;
+           }
+                
+        @endphp
+        @if ($i_error == false && $new_key != null)
+            
+        <tr>
+          <td> 
+           
+            {{ $new_key }} 
+          </td>
+          
+            <td>  {{ $new_row }}  </td>
+        </tr>
+        @endif
             
            
         @endforeach
