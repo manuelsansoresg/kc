fc81fe52c14d89dc9b6f44fcfd57b45b2e69b997
manuelsansoresg <manuelsansoresg@gmail.com>
2023-04-28 12:23:33 -0600

    Mejoras en cómo se muestran las respeustas de la API En credit profile

--- resources/views/panel/kyc/msg.blade.php
+++ resources/views/panel/kyc/msg.blade.php
@@ -2,7 +2,31 @@
     <table class="table">
         @foreach ($data_array as $key =>  $row)
             <tr>
-              <td> {{ $key }} </td>
+              <td> 
+                @if (
+                  $key != 'datosDocProbatorio'
+                  || $key != 'codigoValidacion'
+                  || $key != 'docProbatorio'
+                  || $key != 'datosDocProbatorio'
+                  || $key != 'estatusCurp'
+                  || $key != 'codigoMensaje'
+                  || $key != 'claveMensaje'
+                  || $key != 'codigoValidacion'
+                  || $key != 'estatus'
+                  || $key != 'cic'
+                  || $key != 'numeroEmision'
+                  || $key != 'distritoFederal'
+                  || $key != 'distritoLocal'
+                  || $key != 'ocr'
+                  || $key != 'anioRegistro'
+                  || $key != 'anioEmision'
+                  || $key != 'tipoPersona'
+                  || $key != 'codigoValidacion'
+                  || $key != 'claveMensaje'
+                  )
+                {{ $key }} 
+                @endif
+              </td>
               @if (
                   $key != 'datosDocProbatorio'
                   || $key != 'codigoValidacion'
