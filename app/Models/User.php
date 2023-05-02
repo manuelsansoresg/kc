d83f10cfe424639afbf29cd21d390cf3787adb4d
manuelsansoresg <manuelsansoresg@gmail.com>
2023-05-02 16:43:39 -0600

    Cambios en establecer contraseña por primera vez

--- app/Models/User.php
+++ app/Models/User.php
@@ -130,12 +130,13 @@ class User extends Authenticatable
 
         $token = \Str::random(64);
         $token = str_replace('/', '', $token);
-        $link_password = 'https://app.kaaxclub.com/password/reset/'.$token.'?email='.$find_lead->email;
+        
         PasswordReset::setToken($find_lead->email, $token);
         
         if ($find_user != null) {
              //* send email new account
             if ($is_report == false) {
+                $link_password = 'https://app.kaaxclub.com/password/'.$token.'/'.$find_lead->email.'/change';
                 $domain = 'https://app.kaaxclub.com';
                 $link_login = $domain.'/login';
                 $send_grid = new Csendgrid($find_lead->email, 'creacion cuenta');
@@ -145,6 +146,7 @@ class User extends Authenticatable
             }
 
         } else {
+            $link_password = 'https://app.kaaxclub.com/password/reset/'.$token.'?email='.$find_lead->email;
             unset($data['id']);
             //*create user with leads parameters
             $user = new User($data);
