47d880250b3c92ecd20cebe221cb97aa53bfe65a
manuelsansoresg <manuelsansoresg@gmail.com>
2023-05-09 15:18:27 -0600

    Cambios adicionales en inicio/ayuda

--- resources/views/help.blade.php
+++ resources/views/help.blade.php
@@ -3,11 +3,13 @@
 <div class="hero-wrap sub-header">
     <div class="container">
         <div class="hero-content text-center py-0">
-            <h1 class="hero-title">How can we help?</h1>
+            <h1 class="hero-title">¿Cómo podemos ayudar?</h1>
             <nav aria-label="breadcrumb">
                 <ol class="breadcrumb breadcrumb-s1 justify-content-center mt-3 mb-0">
-                    <li class="breadcrumb-item"><a href="index.html">Home</a></li>
-                    <li class="breadcrumb-item active" aria-current="page">Contact</li>
+                    <li class="breadcrumb-item"><a href="/">Inicio</a></li>
+                    <li class="breadcrumb-item active" aria-current="page">
+                        <a href="/contacto">Contacto</a>
+                    </li>
                 </ol>
             </nav>
         </div><!-- hero-content -->
@@ -22,9 +24,9 @@
                 <section class="cta-section section-space-b bg-pattern mt-5">
                     <div class="container">
                         <div class="cta-box text-center">
-                            <h1 class="cta-title mb-3">Interested in joining us?</h1>
-                            <p class="cta-text mb-4">Hop aboard and view our open positions</p>
-                            <a href="contact.html" class="btn btn-lg btn-dark">See open roles</a>
+                            <h1 class="cta-title mb-3">Conoce nuestro centro de ayuda</h1>
+                            <p class="cta-text mb-4">Haz aclaraciones, preguntas, sugerencias o quejas</p>
+                            <a href="https://yalku.atlassian.net/servicedesk/customer/portal/3" target="_blank" class="btn btn-lg btn-dark">Ir al centro de ayuda</a>
                         </div><!-- end cta-box -->
                     </div><!-- .container -->
                 </section><!-- end cta-section -->
@@ -32,29 +34,33 @@
             <div class="col-lg-5">
                 <div class="contact-info ps-lg-4 ps-xl-5">
                     <div class="section-head-sm">
-                        <h2 class="mb-2">Find Us There</h2>
-                        <p>Collaboratively administrate channels whereas virtual. Objectively seize scalable metrics whereas proactive e-services.</p>
+                        <h2 class="mb-2">Encuéntranos aquí</h2>
+                        <p>También puedes contactarnos en.</p>
                     </div>
                     <ul class="contact-details">
                         <li class="d-flex align-items-center mb-3">
-                            <em class="ni ni-mobile icon-btn icon-btn-s1"></em>
+                            <em class="ni ni-whatsapp icon-btn icon-btn-s1"></em>
+                            {{-- <i class="fa-brands fa-whatsapp"></i> --}}
                             <div class="ms-4">
-                                <strong class="d-block text-black">Phone:</strong>
-                                <span>(123) 123-456 </span>
+                                <strong class="d-block text-black">
+                                    
+                                    WhatsApp:
+                                </strong>
+                                <span><a class="text-decoration-none" target="_blank" href="https://api.whatsapp.com/send?phone=+529999208020&text=hola">9999 20 80 20</a></span>
                             </div>
                         </li>
                         <li class="d-flex align-items-center mb-3">
-                            <em class="ni ni-globe icon-btn icon-btn-s1"></em>
+                            <em class="icon ni ni-facebook-circle icon-btn icon-btn-s1"></em>
                             <div class="ms-4">
-                                <strong class="d-block text-black">Web:</strong>
-                                <a href="https://softnio.com/" target="_blank">www.softnio.com</a>
+                                <strong class="d-block text-black">Facebook:</strong>
+                                <a href="https://www.facebook.com/kaaxclub" target="_blank">facebook.com/kaaxclub</a>
                             </div>
                         </li>
                         <li class="d-flex align-items-center mb-3">
                             <em class="ni ni-mail icon-btn icon-btn-s1"></em>
                             <div class="ms-4">
                                 <strong class="d-block text-black">Email:</strong>
-                                <a href="#">office@softnio.com</a>
+                                <a href="mailto:kaaxclub@gmail.com">kaaxclub@gmail.com</a>
                             </div>
                         </li>
                     </ul>
