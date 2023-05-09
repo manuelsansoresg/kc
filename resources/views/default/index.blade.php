41212a8d416e67f5a7f697322226f2cfe833b2b0
manuelsansoresg <manuelsansoresg@gmail.com>
2023-05-09 17:04:30 -0600

    Cambios adicionales en Home

--- resources/views/default/index.blade.php
+++ resources/views/default/index.blade.php
@@ -1,141 +1,144 @@
 @extends('layouts.default')
 
 @section('hero')
-<div class="hero-wrap hero-wrap-2 section-space">
-    <div class="container">
-        <div class="row align-items-center flex-md-row-reverse justify-content-between">
-            <div class="col-lg-5 col-sm-9 col-md-6">
-                <div class="hero-image">
-                    <img src="images/thumb/nft-img.png" alt="" class="w-100">
-                    <p class="text-center">Elije en menos de 3 minutos</p>
-                </div>
-            </div><!-- end col-lg-5 -->
-            <div class="col-lg-6 col-md-6">
-                <div class="hero-content pb-0 pt-md-0 pe-lg-4">
-                    <h1 class="hero-title mb-4">Encuentra y tramita tu crédito de nómina ideal.
-                    </h1>
-                    <p class="hero-text mb-4 pb-1">Elegir entre todas las financieras puede ser confuso y difícil. Pero no tiene que ser así.
-                    </p>
-                    <!-- button group -->
-                    <ul class="btns-group hero-btns">
-                        <li><a href="https://sprw.io/stt-d1fdfd" target="_blank" class="btn btn-lg btn-dark">Iniciar</a></li>
-                    </ul>
-                    <p class="mt-5 h3">
-                        Fácil, rápido, <span class="text-decoration-underline">sin costo.</span> <i class="fa-solid fa-circle-info"  data-bs-toggle="tooltip" title="Nuestro servicio siempre será sin costo para ti. Nuestro modelo de negocio se basa en una comisión a la financiera que elijas."> </i> 
-                    </p>
-                </div><!-- hero-content -->
-            </div><!-- col-lg-6 -->
-        </div><!-- end row -->
-    </div><!-- .container-->
-</div><!-- end hero-wrap -->
+    <div class="hero-wrap hero-wrap-2 section-space">
+        <div class="container">
+            <div class="row align-items-center flex-md-row-reverse justify-content-between">
+                <div class="col-lg-5 col-sm-9 col-md-6">
+                    <div class="hero-image">
+                        <img src="images/thumb/nft-img.png" alt="" class="w-100">
+                        <p class="text-center">Elije en menos de 3 minutos</p>
+                    </div>
+                </div><!-- end col-lg-5 -->
+                <div class="col-lg-6 col-md-6">
+                    <div class="hero-content pb-0 pt-md-0 pe-lg-4">
+                        <h1 class="hero-title mb-4">Encuentra y tramita tu crédito de nómina ideal.
+                        </h1>
+                        <p class="hero-text mb-4 pb-1">Elegir entre todas las financieras puede ser confuso y difícil. Pero
+                            no tiene que ser así.
+                        </p>
+                        <!-- button group -->
+                        <ul class="btns-group hero-btns">
+                            <li><a href="https://sprw.io/stt-d1fdfd" target="_blank" class="btn btn-lg btn-dark">Iniciar</a>
+                            </li>
+                        </ul>
+                        <p class="mt-5 h3">
+                            Fácil, rápido, <span class="text-decoration-underline">sin costo.</span> <i
+                                class="fa-solid fa-circle-info" data-bs-toggle="tooltip"
+                                title="Nuestro servicio siempre será sin costo para ti. Nuestro modelo de negocio se basa en una comisión a la financiera que elijas.">
+                            </i>
+                        </p>
+                    </div><!-- hero-content -->
+                </div><!-- col-lg-6 -->
+            </div><!-- end row -->
+        </div><!-- .container-->
+    </div><!-- end hero-wrap -->
 @endsection
 
 @section('content')
-<section class="section-space-b feature-section">
-    <div class="container">
-        <div class="section-head text-center">
-            <h2 class="mb-3">Exclusive EnftyMart drops</h2>
-            <p>This is just a simple text made for this unique and awesome template, you can replace it with any text. It is a long established fact.</p>
-        </div><!-- end section-head -->
-        <div class="row g-gs">
-            <div class="col-sm-6 col-md-6 col-lg-3">
-                <a href="product-details-v1.html" class="card card-full">
-                    <img src="images/thumb/art.jpg" class="card-img-top" alt="">
-                    <div class="card-body p-4">
-                        <h5 class="card-title card-title-effect">Seasons by AlexSmith</h5>
-                        <p class="small">AlexSmith's Summer Collection featuring Time.</p>
-                    </div><!-- end card-body -->
-                </a><!-- end card -->
-            </div><!-- end col -->
-            <div class="col-sm-6 col-md-6 col-lg-3">
-                <a href="product-details-v1.html" class="card card-full">
-                    <img src="images/thumb/art-2.jpg" class="card-img-top" alt="">
-                    <div class="card-body p-4">
-                        <h5 class="card-title card-title-effect">Kavin Martin EnftyMart.io</h5>
-                        <p class="small">After a sold-out Art Blocks drop, Martin is back with.</p>
-                    </div><!-- end card-body -->
-                </a><!-- end card -->
-            </div><!-- end col -->
-            <div class="col-sm-6 col-md-6 col-lg-3">
-                <a href="product-details-v1.html" class="card card-full">
-                    <img src="images/thumb/art-3.jpg" class="card-img-top" alt="">
-                    <div class="card-body p-4">
-                        <h5 class="card-title card-title-effect">Kavin Martin EnftyMart.io</h5>
-                        <p class="small">After a sold-out Art Blocks drop, Martin is back with.</p>
-                    </div><!-- end card-body -->
-                </a><!-- end card -->
-            </div><!-- end col -->
-            <div class="col-sm-6 col-md-6 col-lg-3">
-                <a href="product-details-v1.html" class="card card-full">
-                    <img src="images/thumb/art-4.jpg" class="card-img-top" alt="">
-                    <div class="card-body p-4">
-                        <h5 class="card-title card-title-effect">Tyronejkd Universe</h5>
-                        <p class="small">After a sold-out Art Blocks drop, Martin is back with.</p>
-                    </div><!-- end card-body -->
-                </a><!-- end card -->
-            </div><!-- end col -->
-        </div><!-- end row -->
-    </div><!-- end container -->
-</section><!-- end feature-section-->
+    <section class="section-space-b feature-section">
+        <div class="container">
+            <div class="section-head text-center">
+                <h2 class="mb-3">Imparcialidad y confianza</h2>
+                <p>En KaaxClub, no estamos afiliados a ninguna institución financiera. Nos enfocamos en ti y te ofrecemos
+                    opciones transparentes y honestas para que puedas tomar decisiones con confianza.
+                </p>
+            </div><!-- end section-head -->
+            <div class="row g-gs">
+                <div class="col-sm-6 col-md-6 col-lg-3">
+                    <a href="product-details-v1.html" class="card card-full">
+                        <img src="images/thumb/art.jpg" class="card-img-top" alt="">
+                        <div class="card-body p-4">
+                            <h5 class="card-title card-title-effect">Generamos tu reporte (obtén tu mejor resultado)
+                            </h5>
+                            <p class="small">Analizamos y calificamos tus opciones.</p>
+                        </div><!-- end card-body -->
+                    </a><!-- end card -->
+                </div><!-- end col -->
+                <div class="col-sm-6 col-md-6 col-lg-3">
+                    <a href="product-details-v1.html" class="card card-full">
+                        <img src="images/thumb/art-2.jpg" class="card-img-top" alt="">
+                        <div class="card-body p-4">
+                            <h5 class="card-title card-title-effect">Decide</h5>
+                            <p class="small">Elije la mejor opción para ti
+                            </p>
+                        </div><!-- end card-body -->
+                    </a><!-- end card -->
+                </div><!-- end col -->
+                <div class="col-sm-6 col-md-6 col-lg-3">
+                    <a href="product-details-v1.html" class="card card-full">
+                        <img src="images/thumb/art-3.jpg" class="card-img-top" alt="">
+                        <div class="card-body p-4">
+                            <h5 class="card-title card-title-effect">Tramita con nuestra ayuda</h5>
+                            <p class="small">Te brindamos asesoramiento y asistencia en el proceso</p>
+                        </div><!-- end card-body -->
+                    </a><!-- end card -->
+                </div><!-- end col -->
+                <div class="col-sm-6 col-md-6 col-lg-3">
+                    <a href="product-details-v1.html" class="card card-full">
+                        <img src="images/thumb/art-4.jpg" class="card-img-top" alt="">
+                        <div class="card-body p-4">
+                            <h5 class="card-title card-title-effect">Recibe tú crédito
+                            </h5>
+                            <p class="small">Tu dinero estará listo en poco tiempo
+                            </p>
+                        </div><!-- end card-body -->
+                    </a><!-- end card -->
+                </div><!-- end col -->
+            </div><!-- end row -->
+        </div><!-- end container -->
+    </section><!-- end feature-section-->
 
-<section class="section-space how-it-work-section">
-    <div class="container">
-        <div class="section-head text-center">
-            <h2 class="mb-3">Create and sell your NFTs</h2>
-            <p>This is just a simple text made for this unique and awesome template, you can replace it with any text. It is a long established fact.</p>
-        </div><!-- end section-head -->
-        <div class="row g-gs justify-content-center">
-            <div class="col-10 col-sm-6 col-md-6 col-lg-3">
-                <div class="card-htw text-center">
-                    <span class="icon ni ni-wallet icon-lg icon-circle shadow-sm icon-wbg mx-auto mb-4 text-primary"></span>
-                    <h4 class="mb-3">Set up your wallet</h4>
-                    <p class="card-text-s1">Once you’ve set up your wallet of choice, connect it to EnftyMart by clicking the.</p>
-                </div>
-            </div><!-- end col -->
-            <div class="col-10 col-sm-6 col-md-6 col-lg-3">
-                <div class="card-htw text-center">
-                    <span class="icon ni ni-setting icon-lg icon-circle shadow-sm icon-wbg mx-auto mb-4 text-danger"></span>
-                    <h4 class="mb-3">Create collection</h4>
-                    <p class="card-text-s1">Click <a href="profile.html" class="btn-link">My Collections</a> and set up your collection. Add social links, a description.</p>
-                </div>
-            </div><!-- end col -->
-            <div class="col-10 col-sm-6 col-md-6 col-lg-3">
-                <div class="card-htw text-center">
-                    <span class="icon ni ni-camera icon-lg icon-circle shadow-sm icon-wbg mx-auto mb-4 text-info"></span>
-                    <h4 class="mb-3">Add your NFTs</h4>
-                    <p class="card-text-s1">Upload your work (image, video, audio, or 3D art), add a title and description.</p>
-                </div>
-            </div><!-- end col -->
-            <div class="col-10 col-sm-6 col-md-6 col-lg-3">
-                <div class="card-htw text-center">
-                    <span class="icon ni ni-money icon-lg icon-circle shadow-sm icon-wbg mx-auto mb-4 text-success"></span>
-                    <h4 class="mb-3">List them for sale</h4>
-                    <p class="card-text-s1">Choose between auctions, fixed-price listings, and declining-price listings.</p>
-                </div>
-            </div><!-- end col -->
-        </div><!-- end row -->
-    </div><!-- end container -->
-</section><!-- end how-it-work-section -->
-
-<section class="subscibe-section section-space-sm">
-    <div class="container">
-        <div class="join-form-wrap">
-            <div class="row g-gs align-items-center">
-                <div class="col-lg-3">
-                    <h3 class="form-title">Join Our Newsletter</h3>
+    <section class="section-space how-it-work-section">
+        <div class="container">
+            <div class="section-head text-center">
+                <h2 class="mb-3">Ahorra hasta un X %
+                </h2>
+                <p>This is just a simple text made for this unique and awesome template, you can replace it with any text.
+                    It is a long established fact.</p>
+            </div><!-- end section-head -->
+            <div class="row g-gs justify-content-center">
+                <div class="col-10 col-sm-6 col-lg-3">
+                    <div class="card-hiw card-hiw-s3">
+                        <div class="d-flex align-items-center mb-3"><span
+                                class="icon ni ni-wallet icon-md icon-circle icon-wbg me-3 text-blue bg-blue-100"></span>
+                            <h5>Set up your wallet</h5>
+                        </div>
+                        <p class="card-text-s1">Once you’ve set up your wallet of choice, connect it to EnftyMart by
+                            clicking the wallet icon.</p>
+                    </div>
                 </div><!-- end col -->
-                <div class="col-lg-3 col-md-4">
-                    <input class="form-control form-control-s1" type="text" name="name" placeholder="Enter name">
+                <div class="col-10 col-sm-6 col-lg-3">
+                    <div class="card-hiw card-hiw-s3">
+                        <div class="d-flex align-items-center mb-3"><span
+                                class="icon ni ni-file-text icon-md icon-circle icon-wbg me-3 text-purple bg-purple-100"></span>
+                            <h5>Create collection</h5>
+                        </div>
+                        <p class="card-text-s1">Click <a href="profile.html" class="btn-link">My Collections</a> and set up
+                            your collection. Add social links, a description of item.</p>
+                    </div>
                 </div><!-- end col -->
-                <div class="col-lg-3 col-md-4">
-                    <input class="form-control form-control-s1" type="text" name="email" placeholder="Enter email">
+                <div class="col-10 col-sm-6 col-lg-3">
+                    <div class="card-hiw card-hiw-s3">
+                        <div class="d-flex align-items-center mb-3"><span
+                                class="icon ni ni-camera icon-md icon-circle icon-wbg me-3 text-pink bg-pink-100"></span>
+                            <h5>Add your NFTs</h5>
+                        </div>
+                        <p class="card-text-s1">Upload your work (image, video, audio, or 3D art), add a title and
+                            description to item.</p>
+                    </div>
                 </div><!-- end col -->
-                <div class="col-lg-3 col-md-4">
-                    <a href="#" class="btn btn-dark d-md-block">Subscribe Now</a>
+                <div class="col-10 col-sm-6 col-lg-3">
+                    <div class="card-hiw card-hiw-s3">
+                        <div class="d-flex align-items-center mb-3"><span
+                                class="icon ni ni-money icon-md icon-circle icon-wbg me-3 text-orange bg-orange-100"></span>
+                            <h5>List them for sale</h5>
+                        </div>
+                        <p class="card-text-s1">Choose between auctions, fixed-price listings, and declining-price listings
+                            of item.</p>
+                    </div>
                 </div><!-- end col -->
-            </div><!-- row -->
-        </div><!-- end join-form-wrap -->
-    </div><!-- end container -->
-</section><!-- end subscibe-section -->
-
+            </div>
+        </div><!-- end container -->
+    </section><!-- end how-it-work-section -->
 @endsection
