300cbeb8cefc5118f5aad0a67a8a6adce87b4f59
manuelsansoresg <manuelsansoresg@gmail.com>
2023-05-12 15:44:05 -0600

    nav

--- resources/views/layouts/content_report_nav.blade.php
+++ resources/views/layouts/content_report_nav.blade.php
@@ -1,4 +1,4 @@
-<nav class="navbar navbar-expand-lg navbar-transparent navbar-sticky navbar-dark">
+{{-- <nav class="navbar navbar-expand-lg navbar-transparent navbar-sticky navbar-dark">
     <div class="container position-relative">
        
         <div class="text-center">
@@ -30,28 +30,12 @@
 
 
                 </ul>
-                {{-- <ul class="navbar-nav ms-xl-auto">
-                    <li class="nav-item mb-3 mb-lg-0"><a class="btn btn-warning btn-sm hover-lift"
-                            href="demo-request.html">Continuar<span
-                                class="align-middle material-symbols-rounded fs-5 ms-1 d-none d-xl-inline-block">arrow_forward</span></a>
-                    </li>
-                    <li
-                        class="mt-4 mt-lg-0 nav-item d-flex align-items-center justify-content-lg-center flex-lg-column h-100 ms-0 ms-xl-3">
-                        <label
-                            class="dark-mode-checkbox d-flex align-items-center justify-content-center rounded-circle nav-link p-0"
-                            for="ChangeTheme"><input type="checkbox" class="appearance-none" id="ChangeTheme"><span
-                                class="dark-mode-icons size-30 d-inline-flex align-items-center justify-content-center me-2 me-lg-0"><span
-                                    class="material-symbols-rounded align-middle">dark_mode</span><span
-                                    class="material-symbols-rounded align-middle">light_mode</span></span><span
-                                class="ms-1 d-lg-none">Dark Mode</span></label>
-                    </li>
-                </ul> --}}
             </div>
         </div>
     </div>
-</nav>
+</nav> --}}
 
-{{-- <nav class="navbar navbar-expand-lg navbar-transparent navbar-sticky navbar-dark">
+<nav class="navbar navbar-expand-lg navbar-transparent navbar-sticky navbar-dark">
     <div class="container-fluid position-relative">
         <a class="navbar-brand" href="index.html">
             <img src="{{ asset('images/logo-dark.png') }}" class="img-fluid" alt="">
@@ -97,4 +81,4 @@
             </div>
         </div>
     </div>
-</nav> --}}
+</nav>
