3c5cd62fc484e1596e2ee078c288bad0f85f90fc
manuelsansoresg <manuelsansoresg@gmail.com>
2023-05-19 11:56:29 -0600

    Imagen en Generar reporte

--- resources/views/quiz/survey_form.blade.php
+++ resources/views/quiz/survey_form.blade.php
@@ -50,11 +50,19 @@
                 <!-- content @s -->
                 <div class="nk-content" id="content-lead-flex">
                     <div class="d-flex justify-content-center align-items-center vh-100">
+                       
                         <div class="bg-transparent is-dark p-5">
+                            <div class="row justify-content-center">
+                                <div class="col-4  text-center">
+                                    <img class="img-fluid" src="/images/01_reporte.png" alt="">
+                                </div>
+                           </div>
+
                             <div class="text-block">
+                              
                                 <p class="mt-5" data-aos="fade-up" data-aos-duration="9000">
-                                    <h1 class="font-28">Reporte</h1>
-                                    <h5 class="font-15 color-highlight mb-4 " style="color: #6576ff !important;">Vamos a analizar y calificar tus opciones para generar un reporte y puedas elegir tu mejor opción</h5>
+                                    <h1 class="font-28"  style="color: #6576ff !important;">Reporte</h1>
+                                    <h5 class="font-15 color-highlight mb-4 ">Vamos a analizar y calificar tus opciones para generar un reporte y puedas elegir tu mejor opción</h5>
                                     </h3>
                                 </p>
                                 <p class="mt-5">
