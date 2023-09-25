d0e2f74bb810f9048be86f453564277038b8ce7a
manuelsansoresg <manuelsansoresg@gmail.com>
2023-09-25 10:19:27 -0600

    remove

--- app/Strategies/Templates/ControlDeskStrategyTemplate.php
+++ app/Strategies/Templates/ControlDeskStrategyTemplate.php
@@ -2463,9 +2463,6 @@ class ControlDeskStrategyTemplate implements TemplateInterface
         $max_hour           = 20;
         $percent            = self::getPercent($history);
         $color_inf_credit   = 'success';
-        if ($history->id == 181) {
-            dd($history->created_at);
-        }
         $hour               = $history->created_at;
 
         $data_deadline      = deadline($hour, $max_hour, $percent, $color_inf_credit);
