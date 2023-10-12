a30835dde583f2b25ba664c91ec22b6dcf835490
manuelsansoresg <manuelsansoresg@gmail.com>
2023-10-12 17:29:21 -0600

    DKC-119

--- app/Strategies/Templates/AfterMarketStrategyTemplate.php
+++ app/Strategies/Templates/AfterMarketStrategyTemplate.php
@@ -326,6 +326,13 @@ class AfterMarketStrategyTemplate implements TemplateInterface
                     'name' => 'Ver perfil crédito',
                     'icon' => 'icon ni ni-report-profit'
                 ],
+                [
+                    'link' => 'https://wa.me//'.$client->cellphone,
+                    'target' => '_blank',
+                    'onclick' => '',
+                    'name' => 'Whatsapp',
+                    'icon' => 'icon ni ni-whatsapp'
+                ],
                 
             ),
         );
--- resources/views/panel/module/checkup/actions/add_option_dt.blade.php
+++ resources/views/panel/module/checkup/actions/add_option_dt.blade.php
@@ -13,6 +13,7 @@
              <li>
                 <a class="pointer" {{ $options[$i_option]['link'] != '' ? 'href='.$options[$i_option]['link'].'' : '' }}
                         {{ $options[$i_option]['onclick'] != '' ? 'onclick='.$options[$i_option]['onclick'].'' : '' }}
+                        {{ isset($options[$i_option]['target']) != '' ? 'target='.$options[$i_option]['target'].'' : '' }}
                         >
                     <em class="{{ isset($options[$i_option]['icon'])? $options[$i_option]['icon'] : 'icon ni ni-view-list-wd' }}"></em>
                     <span class="{{ isset($options[$i_option]['class']) ? $options[$i_option]['class'] : null }}">{{ $options[$i_option]['name']}}</span></a>
