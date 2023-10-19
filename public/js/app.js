2afb422d0e7c1eb4c4dc01e1dff67e770f94f0ed
manuelsansoresg <manuelsansoresg@gmail.com>
2023-10-19 13:38:54 -0600

    DKC-136

--- public/js/app.js
+++ public/js/app.js
@@ -2160,8 +2160,6 @@ document.addEventListener('DOMContentLoaded', function () {
     }, {
       data: 'label'
     }, {
-      data: 'advisor'
-    }, {
       data: 'status'
     }, {
       data: 'options'
--- resources/js/components/lead/datatable.js
+++ resources/js/components/lead/datatable.js
@@ -46,7 +46,6 @@ document.addEventListener('DOMContentLoaded', function () {
             { data: 'product' },
             { data: 'organizacion' },
             { data: 'label' },
-            { data: 'advisor' },
             { data: 'status' },
             { data: 'options' }
         ],
