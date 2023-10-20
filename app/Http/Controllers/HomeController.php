1cba79080ba548e06e12b1dc1ffbf4942ca1d7aa
manuelsansoresg <manuelsansoresg@gmail.com>
2023-10-20 13:22:07 -0600

    data

--- app/Http/Controllers/HomeController.php
+++ app/Http/Controllers/HomeController.php
@@ -40,7 +40,7 @@ class HomeController extends Controller
 
     public function apiLead(Request $request)
     {
-        ApiLead::create([$request->all()]);
+        ApiLead::create(['data'=> $request->all()]);
     }
     
     public function surveyHola()
