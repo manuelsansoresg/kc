eaf26c99c7b8bba7e6089d82c9a7828429e13cc4
manuelsansoresg <manuelsansoresg@gmail.com>
2023-11-29 16:04:11 -0600

    acciones

--- app/Models/Action.php
+++ app/Models/Action.php
@@ -95,19 +95,25 @@ class Action extends Model
                 });
             })->get();
         
+        //dd($actionsProximasOVencidas);
+
         foreach ($actionsProximasOVencidas as $actionsProximasOVencida) {
             $title = 'Acción prospecto';
-            if ($actionsProximasOVencida->section == 1 ) {
-                $lead = Lead::find($actionsProximasOVencida->id_rel);
-                $name = $lead->name.' '. $lead->last_name;
-            } else {
-                $credit = Credit::find($actionsProximasOVencida->id_rel);
-                $client = $credit->creditClientPerson;
-                $name = $client->name.' '. $client->last_name;
-                $title = 'Acción módulo';
+            try {
+                if ($actionsProximasOVencida->section === 1 ) {
+                    $lead = Lead::find($actionsProximasOVencida->id_rel);
+                    $name = $lead->name.' '. $lead->last_name;
+                } else {
+                    $credit = Credit::find($actionsProximasOVencida->id_rel);
+                    $client = $credit->creditClientPerson;
+                    $name = $client->name.' '. $client->last_name;
+                    $title = 'Acción módulo';
+                }
+            } catch (\Throwable $th) {
+                echo "$actionsProximasOVencida->id_rel <br>";
             }
 
-            if ($lead != null) {
+            /* if ($lead != null) {
                 
                 $type    = isset(config('enums.type_actions')[$actionsProximasOVencida->type])? config('enums.type_actions')[$actionsProximasOVencida->type] : null;
                 $notification_slack = new Slack('kaaxClub', $title.' - '.$type.' - '.$name);
@@ -116,7 +122,7 @@ class Action extends Model
                 $get_action = Action::find($actionsProximasOVencida->id);
                 $get_action->is_notification_slack = 1;
                 $get_action->update();
-            }
+            } */
         }
     }
 
