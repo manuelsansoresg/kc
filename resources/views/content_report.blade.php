9cdd0a51e1116dc818e08595251cbaa4863714ea
manuelsansoresg <manuelsansoresg@gmail.com>
2023-12-01 13:12:56 -0600

    cambiar orden graficas

--- resources/views/content_report.blade.php
+++ resources/views/content_report.blade.php
@@ -92,8 +92,8 @@
         </div>
     </section>
     @php
-        $chart1 = isset($new_financials[1]) ? $new_financials[1] : null;
-        $chart2 = isset($new_financials[0]) ? $new_financials[0] : null;
+        $chart1 = isset($new_financials[0]) ? $new_financials[0] : null;
+        $chart2 = isset($new_financials[1]) ? $new_financials[1] : null;
         $chart3 = isset($new_financials[2]) ? $new_financials[2] : null;
         $chart4 = $my_product_financial;
     @endphp
--- resources/views/content_report_debt.blade.php
+++ resources/views/content_report_debt.blade.php
@@ -8,8 +8,8 @@
 @endsection
 @inject('current_financial_product', 'App\Models\CurrentFinancialProduct')
 @php
-$chart1 = isset($new_financials[1]) ? $new_financials[1] : null;
-$chart2 = isset($new_financials[0]) ? $new_financials[0] : null;
+$chart1 = isset($new_financials[0]) ? $new_financials[0] : null;
+$chart2 = isset($new_financials[1]) ? $new_financials[1] : null;
 $chart3 = isset($new_financials[2]) ? $new_financials[2] : null;
 $chart4 = $my_product_financial;
 @endphp
