ac5dd6f7cc3a886f8b9bfd55ec63e4d61e39252b
manuelsansoresg <manuelsansoresg@gmail.com>
2023-12-04 15:35:38 -0600

    id

--- app/Http/Controllers/Api/ActionManychatController.php
+++ app/Http/Controllers/Api/ActionManychatController.php
@@ -11,7 +11,7 @@ class ActionManychatController extends Controller
     public function store(Request $request)
     {
         $data = $request->all();
-        $manychat_id = $data;
+        $manychat_id = $data['id'];
        /*  $data = array(
             'name' => $request->all()
         ); */
