e3c7022522e8fccb7e314d39272414eb2d84d6d9
manuelsansoresg <manuelsansoresg@gmail.com>
2023-12-07 18:18:31 -0600

    banco

--- app/Http/Controllers/Api/ActionManychatController.php
+++ app/Http/Controllers/Api/ActionManychatController.php
@@ -24,10 +24,7 @@ class ActionManychatController extends Controller
             $data                   = $request->all();
             $manychat_id            = $data['id'];
             $custom_fields          = $data['custom_fields'];
-            //$select_custom_field    = $custom_fields[$head_value];
-            $select_custom_field    = 'manuel';
-
-            
+            $select_custom_field    = $custom_fields[$head_value];
             
             self::saveOrganization($section, $select_head, $select_custom_field, $manychat_id);
             self::saveBank($section, $select_head, $select_custom_field, $manychat_id);
