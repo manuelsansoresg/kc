c67cffbefaeaac854f77ecd6e254809d9a6230e1
manuelsansoresg <manuelsansoresg@gmail.com>
2024-01-02 16:51:57 -0600

    DKC-274

--- resources/views/layouts/report.blade.php
+++ resources/views/layouts/report.blade.php
@@ -26,7 +26,7 @@
     <!--:Main style:-->
     <link rel="stylesheet" href="{{ asset('/assets_report/css/theme.min.css') }}">
     <link rel="stylesheet" href="{{ asset('/assets_report/css/app.css') }}">
-    <link rel="stylesheet" href="https://cdn.jsdelivr.net/npm/select2@4.0.13/dist/css/select2.min.css" />
+    <link href="https://cdn.jsdelivr.net/npm/select2@4.1.0-rc.0/dist/css/select2.min.css" rel="stylesheet" />
     <link rel="stylesheet" href="https://cdn.jsdelivr.net/npm/select2-bootstrap-5-theme@1.3.0/dist/select2-bootstrap-5-theme.min.css" />
     <!-- Or for RTL support -->
     <link rel="stylesheet" href="https://cdn.jsdelivr.net/npm/select2-bootstrap-5-theme@1.3.0/dist/select2-bootstrap-5-theme.rtl.min.css" />
@@ -137,7 +137,7 @@
     
     <script src="https://cdn.jsdelivr.net/npm/sweetalert2@11"></script>
     <script src="https://cdn.jsdelivr.net/npm/bootstrap@5.1.3/dist/js/bootstrap.bundle.min.js"></script>
-    <script src="https://cdn.jsdelivr.net/npm/select2@4.0.13/dist/js/select2.full.min.js"></script>
+    <script src="https://cdn.jsdelivr.net/npm/select2@4.1.0-rc.0/dist/js/select2.min.js"></script>
     
     @yield('add_script')
     
--- resources/views/report/modal_product.blade.php
+++ resources/views/report/modal_product.blade.php
@@ -1,6 +1,6 @@
 <div class="modal fade " id="modal-product" tabindex="-1" aria-modal="true" role="dialog">
     <div class="modal-dialog modal-dialog-centered modal-lg" role="document">
-        <div class="modal-content bg-dark"><a href="#" class="close" data-bs-dismiss="modal"><em
+        <div class="modal-content bg-dark2"><a href="#" class="close" data-bs-dismiss="modal"><em
                     class="icon ni ni-cross-sm"></em></a>
             <div class="modal-body modal-body-lg">
                 <form action="" id="frm-modal-product">
@@ -10,15 +10,15 @@
 
                     <div class="mb-3">
                         <label for="exampleFormControlInput1" class="form-label">Crédito(s) actual(es).</label>
-                        <select class="form-select select2multiple" multiple="multiple" name="products[]"   data-search="on">
+                        <select class="form-select select2multiple" multiple="multiple" name="products[]"   data-search="on" data-placeholder="Escribe para buscar">
                             @foreach ($financial_products as $product)
                             <option value="{{ $product->id }}"> {{ $product->commercial_name}} - {{ $product->name }} </option>
                         @endforeach
                         </select>
                       </div>
                     <div class="col-12 text-center mt-5">
-                        <a href="#" class="btn btn-outline-secondary" onclick="cancelModalProduct()">No veo mi crédito</a>
-                        <a href="#" class="btn btn-outline-secondary" onclick="continueModalProduct()">Continuar</a>
+                        <a href="#" class="btn btn-outline-primary" onclick="cancelModalProduct()">No veo mi crédito</a>
+                        <a href="#" class="btn btn-outline-primary" onclick="continueModalProduct()">Continuar</a>
                     </div>
                 </form>
             </div>
