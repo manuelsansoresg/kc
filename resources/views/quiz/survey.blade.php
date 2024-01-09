f6d7e604d56bb14017bd2991fdb2160b0d2c3d72
manuelsansoresg <manuelsansoresg@gmail.com>
2024-01-09 10:09:27 -0600

    DKC-298

--- resources/views/quiz/survey.blade.php
+++ resources/views/quiz/survey.blade.php
@@ -50,8 +50,10 @@
                             </a>
                             <div class="text-block" data-aos="fade-up"
                             data-aos-duration="5000">
-                                <img class="nk-survey-gfx mb-5" src="{{ asset('images/apple-touch-icon.png') }}"
-                                    alt="">
+                                <div class="d-none d-md-block">
+                                    <img class="nk-survey-gfx mb-5" src="{{ asset('images/apple-touch-icon.png') }}"
+                                        alt="">
+                                </div>
                                 <h3 class="text-white">Encuesta de satisfacción</h3>
                                 <p>Tú opinion es muy importante, nos ayuda a mejorar.</p>
                             </div>
