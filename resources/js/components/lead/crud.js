ec2139c76bdb328195984d4d6f758d018f48254d
manuelsansoresg <manuelsansoresg@gmail.com>
2024-01-29 12:02:28 -0600

    guardar otro en prospecto agreement

--- resources/js/components/lead/crud.js
+++ resources/js/components/lead/crud.js
@@ -16,7 +16,7 @@ window.organizationChange = function(lead_agreement_id, financial_id, other){
         lead_agreement_id = 0;
         $('#new_agreement').val(other);
     }
-
+    //alert(lead_agreement_id);
     if (lead_agreement_id != null) {
         $('#lead-agreement').val(lead_agreement_id).trigger("change");
     }
