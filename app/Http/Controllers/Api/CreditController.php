e597378a66445d633bc98cac03c19f0dc770f2ca
manuelsansoresg <manuelsansoresg@gmail.com>
2024-03-21 16:47:50 -0600

    desactivar delivery

--- app/Http/Controllers/Api/CreditController.php
+++ app/Http/Controllers/Api/CreditController.php
@@ -5,6 +5,7 @@ namespace App\Http\Controllers\Api;
 use App\Http\Controllers\Controller;
 use App\Models\Credit;
 use App\Models\HistoryLog;
+use App\Strategies\Values\SendNotificationsValues;
 use Illuminate\Http\Request;
 
 class CreditController extends Controller
@@ -17,12 +18,28 @@ class CreditController extends Controller
     public function activar(Credit $credit, $s2_credit_id, $tipo = 1)
     {
         $statusMove = HistoryLog::CREDITS_DELIVERED;
+        $credit_id = $credit->id;
         if ($tipo == 2) {
             $statusMove = HistoryLog::CREDIT_REJECTED;
         }
         HistoryLog::move($credit->id, $statusMove, $statusMove);
-        Credit::where('id', $credit->id)->update([
+        Credit::where('id', $credit_id)->update([
             's2_credit_id' => $s2_credit_id
+        ]);
+        //desactivar de delivery
+        HistoryLog::updateStatusProgress(HistoryLog::KC_DELIVERY_FORM_STEP_4, $credit_id, 1);
+        //*inicializar las acciones de la siguiente etapa en curso
+        HistoryLog::move($credit_id, HistoryLog::KC_PAYMENT, HistoryLog::KC_PAYMENT, null, false);
+        // send push
+        $notification_add   = SendNotificationsValues::STRATEGY['pushCreditKcPayment'];
+        (new $notification_add)->send($credit->id);
+        
+        HistoryLog::updateStatusProgress(HistoryLog::KC_PAYMENT, $credit_id, 0);
+        $credit->delivered = 1;
+        //desactivate delivery
+        HistoryLog::where(['id_rel' => $credit_id, 'status_id' => HistoryLog::KC_DELIVERY, 'status' => 1])
+        ->update([
+           'status' => 0
         ]);
     }
 }
