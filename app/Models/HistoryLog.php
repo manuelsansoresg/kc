ab2b00bf3656a1007573e5e90b0b7e8cd38c3c0f
manuelsansoresg <manuelsansoresg@gmail.com>
2024-04-08 12:02:02 -0600

    DKC-411

--- app/Models/HistoryLog.php
+++ app/Models/HistoryLog.php
@@ -478,6 +478,9 @@ class HistoryLog extends Model
             HistoryLog::move($id_rel, HistoryLog::KC_WALLET_ADD_UPLOAD, HistoryLog::KC_WALLET_ADD_UPLOAD);
             
             HistoryLog::updateStatusProgress(HistoryLog::KC_WALLET_ADD_UPLOAD, $id_rel, 0);
+
+            $notification_slack = new Slack('kaaxClub', 'KC Wallet - Solicitud de agregar fondos');
+            $notification_slack->sendMessage();
         }
 
         if ($status_id == HistoryLog::KC_AFTER_MARKET) {
