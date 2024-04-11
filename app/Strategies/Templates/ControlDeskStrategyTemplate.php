dbf2c5ba64a638f67a13f514dd5477aa6b4a3509
manuelsansoresg <manuelsansoresg@gmail.com>
2024-04-11 10:17:43 -0600

    error

--- app/Strategies/Templates/ControlDeskStrategyTemplate.php
+++ app/Strategies/Templates/ControlDeskStrategyTemplate.php
@@ -2190,7 +2190,7 @@ class ControlDeskStrategyTemplate implements TemplateInterface
 
                 HistoryLog::move($credit->id, HistoryLog::KC_CONTROL_DESK_FORM_STEP_2, HistoryLog::KC_CONTROL_DESK_FORM_STEP_2, null, false);
                 HistoryLog::updateStatusProgress(HistoryLog::KC_CONTROL_DESK_FORM_STEP_2, $credit->id, 0);
-                Credit::setTotalCapital($credit->id);
+                //Credit::setTotalCapital($credit->id);
             }
 
             if ($percent_form_step2 == 100) {
