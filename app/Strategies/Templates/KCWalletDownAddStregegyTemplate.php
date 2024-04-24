d9f5323e4abe1326f04473cc6279538c9106b896
manuelsansoresg <manuelsansoresg@gmail.com>
2024-04-24 12:47:14 -0600

    0 default

--- app/Strategies/Templates/KCWalletDownAddStregegyTemplate.php
+++ app/Strategies/Templates/KCWalletDownAddStregegyTemplate.php
@@ -124,7 +124,7 @@ class KCWalletDownAddStregegyTemplate implements TemplateInterface
         $optionInvestor = $is_investor === true ? $getInvestor->id : $users;
 
         $format_withdraw_available = 'Disponible para el retiro:';
-        $withdraw_available = $getInvestor->withdraw_available > 0 ?  $getInvestor->withdraw_available : 0;
+        $withdraw_available = $getInvestor != null && $getInvestor->withdraw_available > 0 ?  $getInvestor->withdraw_available : 0;
         if ($getInvestor != null) {
             $format_withdraw_available .= format_price($withdraw_available);
         }
