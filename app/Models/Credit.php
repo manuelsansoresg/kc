bb19ad4bf4d538585b55a89a9c8f4fbe54307833
manuelsansoresg <manuelsansoresg@gmail.com>
2024-04-29 16:38:15 -0600

    openning comision

--- app/Models/Credit.php
+++ app/Models/Credit.php
@@ -117,8 +117,8 @@ class Credit extends Model
         $financialProduct = FinancialProduct::find($credit->applied_financial_product);
         //actualizar
         if ($credit!= null && $financialProduct != null) {
-            $net_amount = $credit->applied_import - $credit->opening_commission - $credit->refinance_adjustment - $credit->third_party_adjustment;
             $openin_commission = $credit->applied_import * ($financialProduct->opening_commission_rate / 100);
+            $net_amount = $credit->applied_import - $openin_commission - $credit->refinance_adjustment - $credit->third_party_adjustment;
             $sod_commission = $financialProduct->sod_commission_amount;
             Credit::where('id', $credit->id)->update([
                 'opening_commission' => $openin_commission,
