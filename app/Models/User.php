b616c75109b6ea3267016878867cfd936ec20be5
manuelsansoresg <manuelsansoresg@gmail.com>
2024-07-11 11:35:44 -0600

    investor

--- app/Models/User.php
+++ app/Models/User.php
@@ -170,14 +170,16 @@ class User extends Authenticatable
             $userId = $user->id;
             // Buscar el inversor asociado al usuario
             $investor = Investor::where('user_id', $userId)->first();
-            if ($investor != null) {
-                InvestorProduct::where('investor_id', $investor->id)->delete();
-                foreach ($arrayFinancialProductsId as $arrayFinancialProductId) {
-                    InvestorProduct::create([
-                        'investor_id' => $investor->id,
-                        'financial_products_id' => $arrayFinancialProductId
-                    ]);
-                }
+            
+            if ($investor == null) {
+                $investor = Investor::create(['user_id' => $userId]);
+            } 
+            InvestorProduct::where('investor_id', $investor->id)->delete();
+            foreach ($arrayFinancialProductsId as $arrayFinancialProductId) {
+                InvestorProduct::create([
+                    'investor_id' => $investor->id,
+                    'financial_products_id' => $arrayFinancialProductId
+                ]);
             }
 
             
