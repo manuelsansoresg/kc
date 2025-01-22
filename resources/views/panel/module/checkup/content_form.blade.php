c5c5f61917bd8ba1f76c1f7ec014e3a4e44e581f
manuelsansoresg <manuelsansoresg@gmail.com>
2025-01-22 12:36:07 -0600

    DKC-729

--- resources/views/panel/module/checkup/content_form.blade.php
+++ resources/views/panel/module/checkup/content_form.blade.php
@@ -48,22 +48,22 @@
                             <div class="card">
                                 <div class="card-body">
                                     <span>
-                                        <span class="fw-bold">NOMBRE :</span> {{ $credit->client->id }} - {{ $credit->client->name }} {{ $credit->client->last_name }} {{ $credit->client->second_last_name }}
-                                        <br> <span class="fw-bold">RFC :</span>  {{ $credit->client->rfc }}
-                                        <br><span class="fw-bold">Organización :</span>  {{ $credit->client->agreement->name }}
-                                        <br><span class="fw-bold">Tel :</span> : {{ $credit->client->cellphone }}
+                                        <span class="fw-bold">Nombre:</span> {{ $credit->client->id }} - {{ $credit->client->name }} {{ $credit->client->last_name }} {{ $credit->client->second_last_name }}
+                                        <br> <span class="fw-bold">RFC:</span>  {{ $credit->client->rfc }}
+                                        <br><span class="fw-bold">Organización:</span>  {{ $credit->client->agreement->name }}
+                                        <br><span class="fw-bold">Tel:</span>  {{ $credit->client->cellphone }}
                                         <br><br>
-                                        <span class="fw-bold">Producto :</span> {{ $product->alias }}
-                                        <br><span class="fw-bold">Monto :</span> {{ format_price($credit->applied_import) }}
-                                        <br><span class="fw-bold">Plazo :</span> {{ format_price($credit->applied_term) }}
-                                        <br><span class="fw-bold">Pago :</span> {{ format_price($credit->applied_loan_discount) }}
-                                        <br><span class="fw-bold">Periodicidad :</span> {{ $periodicity }}
-                                        <br><span class="fw-bold">Tipo de crédito :</span> {{ $tipoCredito->alias}}
-                                        <br><span class="fw-bold">Compra cartera :</span> {{ format_price($credit->third_party_adjustment) }}
-                                        <br><span class="fw-bold">Promotor :</span> {{ $getAsesor != null ? $getAsesor->name.' '.$getAsesor->last_name.' '.$getAsesor->second_last_name : null}}
-                                        <br><span class="fw-bold">Origen :</span> {{ $origin }}
-                                        <br><span class="fw-bold">Etiquetas :</span> {{ $tags }}
-                                        <br><span class="fw-bold">Ultimo comentario :</span> {{ $lastComment!= null ? $lastComment->comment : null }}
+                                        <span class="fw-bold">Producto:</span> {{ $product->alias }}
+                                        <br><span class="fw-bold">Monto:</span> {{ format_price($credit->applied_import) }}
+                                        <br><span class="fw-bold">Plazo:</span> {{ format_price($credit->applied_term) }}
+                                        <br><span class="fw-bold">Pago:</span> {{ format_price($credit->applied_loan_discount) }}
+                                        <br><span class="fw-bold">Periodicidad:</span> {{ $periodicity }}
+                                        <br><span class="fw-bold">Tipo de crédito:</span> {{ $tipoCredito->alias}}
+                                        <br><span class="fw-bold">Compra cartera:</span> {{ format_price($credit->third_party_adjustment) }}
+                                        <br><span class="fw-bold">Promotor:</span> {{ $getAsesor != null ? $getAsesor->name.' '.$getAsesor->last_name.' '.$getAsesor->second_last_name : null}}
+                                        <br><span class="fw-bold">Origen:</span> {{ $origin }}
+                                        <br><span class="fw-bold">Etiquetas:</span> {{ $tags }}
+                                        <br><span class="fw-bold">Ultimo comentario:</span> {{ $lastComment!= null ? $lastComment->comment : null }}
                                         <hr>
                                         <span class="fw-bold"> Documentos:</span>
                                        
