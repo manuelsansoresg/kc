f5c5ee75e0c66017a14d180c0a72df94148baf77
manuelsansoresg <manuelsansoresg@gmail.com>
2025-02-10 12:08:18 -0600

    DKC-780

---
+++ app/Lib/Cemail.blade.php
@@ -0,0 +1,55 @@
+<?php
+namespace App\Lib;
+use Illuminate\Support\Facades\Mail;
+
+class Cemail
+{
+    public $to;
+    public $from;
+    public $subject;
+    public $content;
+    public $template;
+    public $cc;
+
+    public function __construct($to = null, $template, $subject = '', $content= '', $cc= '')
+    {
+        $this->to       = $to;
+        $this->subject  = $subject;
+        $this->content  = $content;
+        if ($content == '') {
+            $this->content  = array('title' => $this->subject);
+        } else {
+            // Si el content no tiene la clave 'title', se agrega
+            if (!isset($this->content['title'])) {
+                $this->content['title'] = $this->subject;
+            }
+        }
+        $this->template = $template;
+        $this->cc       = $cc;
+    }
+
+    public function sendEmail()
+    {
+        // Convertir la lista de destinatarios en un array
+        if ($this->to === null) {
+            $getNotification = EmailNotification::find(1);
+            $recipients = array_map('trim', explode(',', $getNotification->email));
+        } else {
+            $recipients = array_map('trim', explode(',', $this->to));
+        }
+        
+        
+        // Preparar el correo utilizando la vista en la carpeta "template"
+        Mail::send("email.{$this->template}", $this->content, function ($message) use ($recipients) {
+            $message->to($recipients); // Enviar a múltiples destinatarios
+
+            if ($this->cc) {
+                // Si hay direcciones en 'cc', convertirlas en array y añadirlas
+                $ccRecipients = array_map('trim', explode(',', $this->cc));
+                $message->cc($ccRecipients);
+            }
+
+            $message->subject($this->subject);
+        });
+    }
+}
---
+++ resources/views/email/demo1.blade.php
@@ -0,0 +1,35 @@
+@extends('layouts.template_email')
+
+@section('content')
+<table style="width:100%;max-width:620px;margin:0 auto;background-color:#ffffff;">
+    <tbody>
+          <tr>
+            <td style="padding: 30px 30px 15px 30px;">
+                <h2 style="font-size: 18px; color: #6576ff; font-weight: 600; margin: 0;">Tus fondos están listos para ser prestados.</h2>
+            </td>
+        </tr>
+        <tr>
+            <td style="padding: 0 30px 20px">
+                <p style="margin-bottom: 10px;">Hola {{name}}.</p>
+               Hemos validado tus fonfos con éxito. Para prestarlos, tienes que ir a tu cuenta y en la sección de Resumen presiona "Prestar". No dudes en consultarnos por cualquier duda.</p>
+               <br><br>
+                                                        
+                   
+                <a href="{{link_account}}" style="background-color:#6576ff;border-radius:4px;color:#ffffff;display:inline-block;font-size:13px;font-weight:600;line-height:44px;text-align:center;text-decoration:none;text-transform: uppercase; padding: 0 30px">Ir a PRestar</a>
+                   
+            </td>
+        </tr>
+        <tr>
+            <td style="padding: 0 30px">
+               
+               
+            </td>
+        </tr>
+        <tr>
+            <td style="padding: 20px 30px 40px">
+              
+            </td>
+        </tr>
+    </tbody>
+</table>
+@endsection
