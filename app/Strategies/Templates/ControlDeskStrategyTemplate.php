d8603a27813f2fafe6e571ae4dd6e0c906bc910a
manuelsansoresg <manuelsansoresg@gmail.com>
2025-02-11 10:36:17 -0600

    DKC-788

--- app/Strategies/Templates/ControlDeskStrategyTemplate.php
+++ app/Strategies/Templates/ControlDeskStrategyTemplate.php
@@ -18,6 +18,7 @@ use App\Models\HistoryLog;
 use App\Models\InvestorsCredit;
 use App\Models\Lead;
 use App\Models\Product;
+use App\Models\Transaction;
 use App\Models\User;
 use App\Strategies\TemplateInterface;
 use App\Strategies\Values\SendNotificationsValues;
@@ -4271,6 +4272,10 @@ class ControlDeskStrategyTemplate implements TemplateInterface
                         
                         if ($percentTask4Step3 == 100) {
                             InvestorsCredit::saveEdit($credit->id);
+                            $getInvestors = InvestorsCredit::where('credit_id', $credit->id)->get();
+                            foreach ($getInvestors as $getInvestor) {
+                                Transaction::setTotalCapital($getInvestor->investor_id);
+                            }
                             HistoryLog::updateStatusProgress(HistoryLog::KC_CONTROL_DESK_TASK4_STEP3, $credit->id, 1); //terminar tarea
 
                             HistoryLog::move($credit->id, HistoryLog::KC_CONTROL_DESK_TASK5_STEP3, HistoryLog::KC_CONTROL_DESK_TASK5_STEP3, null, false);
