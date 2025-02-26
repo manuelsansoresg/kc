7b07460791ed3916f24ae87cb8fb768424e49968
manuelsansoresg <manuelsansoresg@gmail.com>
2025-02-26 10:43:14 -0600

    DKC-817

--- resources/views/panel/module/checkup/product.blade.php
+++ resources/views/panel/module/checkup/product.blade.php
@@ -1,6 +1,4 @@
-<a  class="project-title">
-    <div class="preview-icon-wrap text-gray mt-1"></div>
+<div class="preview-icon-wrap text-gray mt-1"></div>
     <div class="project-info">
         <p class=""> &nbsp; {{ $alias_product }}</p>
-    </div>
-</a>
+</div>
