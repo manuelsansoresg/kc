f03eb0a16b64c7bbb835ea00c23f0e64e3b056ef
manuelsansoresg <manuelsansoresg@gmail.com>
2025-03-05 15:04:35 -0600

    DKC-842

--- database/seeders/PermissionSeeder.php
+++ database/seeders/PermissionSeeder.php
@@ -14,6 +14,7 @@ class PermissionSeeder extends Seeder
      */
     public function run()
     {
+        Permission::create(['name' => 'Administración']);
         Permission::create(['name' => 'RRHH']);
     }
 }
