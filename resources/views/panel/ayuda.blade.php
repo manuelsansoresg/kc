9cbdfddd91ab8a3c4bee24d38e3b39579c0eea91
manuelsansoresg <manuelsansoresg@gmail.com>
2025-03-12 15:10:13 -0600

    DKC-846

--- resources/views/panel/ayuda.blade.php
+++ resources/views/panel/ayuda.blade.php
@@ -46,9 +46,9 @@
                             <section class="cta-section section-space-b bg-pattern mt-5">
                                 <div class="container">
                                     <div class="cta-box text-center">
-                                        <h1 class="cta-title mb-3">Atención al personal</h1>
+                                        <h1 class="cta-title mb-3">Atención  personal</h1>
                                         <p class="cta-text mb-4">Haz aclaraciones, preguntas, sugerencias o quejas</p>
-                                        <a href="https://test.kaaxclub.com/panel/ayuda" target="_blank" class="btn btn-lg btn-dark">Ir al centro de ayuda</a>
+                                        <a href="https://wa.me/5219999208020?text=Contacto%20o%20ayuda%20Kaaxclub" target="_blank" class="btn btn-lg btn-dark">Ir al chat &nbsp; <em class="icon ni ni-whatsapp"></em></a>
                                     </div><!-- end cta-box -->
                                 </div><!-- .container -->
                             </section><!-- end cta-section -->
