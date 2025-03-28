983f2f381fdef52b8c0e78a29c9dbca67bba866b
manuelsansoresg <manuelsansoresg@gmail.com>
2025-03-28 13:09:08 -0600

    Fix typo in commission calculation in wallet resumen view

--- resources/views/panel/module/wallet/resumen.blade.php
+++ resources/views/panel/module/wallet/resumen.blade.php
@@ -311,7 +311,7 @@
                                 $totalIngresos     = $recursosFondeados + $totalCollected;
                                 
                                 $prestamosRealizados       = $investor != null ? $investor->total_capital : 0;
-                                $comisionesPagadas         = $investor != null ? $investor->collections_commission  + $investor->iva_commmission : 0;
+                                $comisionesPagadas         = $investor != null ? $investor->collection_commission  + $investor->iva_commission : 0;
                                 $recursosRetirados         = $investor != null ? $investor->withdrawn_money : 0;
                                 $perdidasporCarteraVencida = 0;
                                 $totalEgresos              = $prestamosRealizados + $comisionesPagadas + $recursosRetirados + $perdidasporCarteraVencida;
