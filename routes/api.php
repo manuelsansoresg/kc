9cea7d7e477af66de96f30abced0143e315fdfbe
manuelsansoresg <manuelsansoresg@gmail.com>
2025-04-01 17:28:03 -0600

    DKC-930

--- routes/api.php
+++ routes/api.php
@@ -49,4 +49,4 @@ Route::post('set-url-rfc', [ActionManychatController::class, 'setUrlRfc']);
 Route::post('validate-cliente-activo', [ActionManychatController::class, 'validateClienteActivo']);
 Route::post('validate-tramite-pendiente', [ActionManychatController::class, 'validateTramitePendiente']);
 Route::post('validate-identity', [ActionManychatController::class, 'validateIdentity']);
-Route::get('validate-identity/get', [ActionManychatController::class, 'getValidateIdentity']);
+Route::post('validate-identity-get', [ActionManychatController::class, 'getValidateIdentity']);
