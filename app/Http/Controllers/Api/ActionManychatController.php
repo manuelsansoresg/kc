1ede24f362b3da4a5f882626870d332f33ac1151
manuelsansoresg <manuelsansoresg@gmail.com>
2025-04-03 11:17:07 -0600

    Refactor setSod method in ActionManychatController to return productId and agreement_id in response, while commenting out the Lead update logic.

--- app/Http/Controllers/Api/ActionManychatController.php
+++ app/Http/Controllers/Api/ActionManychatController.php
@@ -412,14 +412,14 @@ class ActionManychatController extends Controller
 
         $getProduct = Product::where('alias', $products[$productId])->first();
         $getFinancialProduct = FinancialProduct::select('financial_products.id')
-        
+
         ->join('financial_agreements', 'financial_agreements.product_id', 'financial_products.id')
         ->where('type_product_id', $getProduct->id)
         ->where('financial_agreements.agreement_id', $getLead->agreement_id)
         ->first();
-        Lead::where('manychat_id', $manychat_id)->update([
+        /* Lead::where('manychat_id', $manychat_id)->update([
             'product_id' => $getFinancialProduct->id,
-        ]);
-        return response()->json(['validate' => true]);
+        ]); */
+        return response()->json(['validate' => true, 'productId' => $getProduct->id, 'agreement_id' => $getLead->agreement_id]);
     }
 }
