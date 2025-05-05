9ebdee8917e1f051d6970adfce630835b6439fd2
manuelsansoresg <manuelsansoresg@gmail.com>
2025-05-05 12:26:00 -0600

    Enhance response in validateSodActive method of ActionManychatController to include 'getClientPerson' in the JSON output, improving data returned for client validation.

--- app/Http/Controllers/Api/ActionManychatController.php
+++ app/Http/Controllers/Api/ActionManychatController.php
@@ -406,7 +406,7 @@ class ActionManychatController extends Controller
             'SOD - Activo' => $sod_active,
         );
         $manychat->setCustomFields($dataField, $manychat_id);
-        return response()->json(['validate' => $sod_active]);
+        return response()->json(['validate' => $sod_active, 'getClientPerson' => $getClientPerson]);
     }
 
     public function validateFechasPermitidas(Request $request)
