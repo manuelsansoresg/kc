71d4a555de09639989bc68e8dc85a46ab1d09a04
manuelsansoresg <manuelsansoresg@gmail.com>
2025-05-30 10:16:56 -0600

    Add getHour method to HomeController and update routes for hour retrieval

--- app/Http/Controllers/HomeController.php
+++ app/Http/Controllers/HomeController.php
@@ -39,6 +39,13 @@ class HomeController extends Controller
     {
     }
 
+    public function getHour()
+    {
+        $hour =  date('Y-m-d H:i:s');
+        $zone = date_default_timezone_get();
+        return response()->json(['hour' => $hour, 'zone' => $zone]);
+    }
+
     /**
      * Show the application dashboard.
      *
