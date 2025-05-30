4096a1ee754fdc53c0185397848e9e9c4f18fb87
manuelsansoresg <manuelsansoresg@gmail.com>
2025-05-30 11:08:31 -0600

    Update timezone configuration from 'Mexico/General' to 'America/Mexico_City'

--- config/app.php
+++ config/app.php
@@ -67,7 +67,7 @@ return [
     |
     */
 
-    'timezone' => 'Mexico/General',
+    'timezone' => 'America/Mexico_City',
 
     /*
     |--------------------------------------------------------------------------
