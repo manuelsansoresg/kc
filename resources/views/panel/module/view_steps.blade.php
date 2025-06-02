5959676922c11f97baeaa30dabe3cda6f3145b32
manuelsansoresg <manuelsansoresg@gmail.com>
2025-06-02 13:41:10 -0600

    Remove debug dump of credit variable and update conditional rendering in view_steps.blade.php

--- resources/views/panel/module/view_steps.blade.php
+++ resources/views/panel/module/view_steps.blade.php
@@ -172,8 +172,7 @@
                                         @endif
                                     @endif
                                 </div>
-                                {{ dd($credit) }}
-                                @if ($credit != null)
+                                @if ($model != 'wallet')
                                 <!-- Columna secundaria: Card info cliente -->
                                 <div class="col-md-3 d-none d-md-block offset-md-1">
                                     <div class="card">
