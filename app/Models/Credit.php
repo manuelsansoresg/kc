4e06ecdf8eb3a618a88e888fdfd7d7c71cd8c0b9
manuelsansoresg <manuelsansoresg@gmail.com>
2025-06-17 09:38:30 -0600

    Revertir cambios

--- app/Models/Credit.php
+++ app/Models/Credit.php
@@ -9,6 +9,7 @@ use Illuminate\Database\Eloquent\Factories\HasFactory;
 use Illuminate\Database\Eloquent\Model;
 use Illuminate\Support\Facades\Auth;
 
+
 class Credit extends Model
 {
     use HasFactory;
