2cd677cc8c82cc839d28190f3c0f087477fdfbe0
manuel.sansores <manuelsansoresg@gmail.com>
2025-08-22 09:43:46 -0600

    DKC-1078

--- resources/views/layouts/admin.blade.php
+++ resources/views/layouts/admin.blade.php
@@ -513,7 +513,7 @@
                             </div><!-- .nk-header-brand -->
 
                             @if ( Request::segment(2) != 'inversionista' &&  Request::segment(2) != 'kc-wallet' && Request::segment(2) != 'kc-down-wallet' && Request::segment(2) != 'ayuda' )
-                            <div class="nk-header-search ms-3 ms-xl-0">
+                            <div class="nk-header-search ms-3 ms-xl-0 d-none">
                                 <em class="icon ni ni-search" id="icon-search"></em>
                                 <form action="/panel/user/search/view" method="GET">
                                     <input type="text"  name="query" id="query" value="{{ old('query') }}"  class="form-control border-transparent form-focus-none" placeholder="Buscar ..">
