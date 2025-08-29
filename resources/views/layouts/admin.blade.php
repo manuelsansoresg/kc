077e236c7a788a8566de65f3a0c22ce662104b60
manuel.sansores <manuelsansoresg@gmail.com>
2025-08-29 10:13:26 -0600

    fix(admin): correct conditional logic for menu item display
    The menu items for "Mis colaboradores" and "Solicitudes" were incorrectly nested under the same condition. Separated the conditions to ensure proper display based on individual permissions.

--- resources/views/layouts/admin.blade.php
+++ resources/views/layouts/admin.blade.php
@@ -360,14 +360,17 @@
                                         </li><!-- .nk-menu-item -->
                                     @endhasrole
                                     @hasrole('Cliente inversionista')
-                                    @if (auth()->user()->can('Administración') || auth()->user()->can('Gestionar colaboradores'))
+                                    @if (auth()->user()->can('Administración'))
                                     <li class="nk-menu-item">
                                     <a href="/panel/clients/colaboradores/show" class="nk-menu-link">
                                         {{-- <span class="nk-menu-icon"><em class="icon ni ni-happy"></em></span> --}}
                                         <span class="nk-menu-text"> <i class="fa-solid fa-users"></i> Mis colaboradores</span>
                                     </a>
                                     </li><!-- .nk-menu-item -->
-                                     <li class="nk-menu-item">
+                                   
+                                    @endif
+                                    @if (auth()->user()->can('Gestionar colaboradores'))
+                                          <li class="nk-menu-item">
                                             <a href="/panel/solicitud" class="nk-menu-link">
                                                 <span class="nk-menu-text"><em class="icon ni ni-clipboad-check"></em> Solicitudes </span>
                                             </a>
