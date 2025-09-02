e8b4f427be6a2cf43dde90511b74eae10797aab0
manuel.sansores <manuelsansoresg@gmail.com>
2025-09-02 13:02:15 -0600

    fix: remove hardcoded currency symbols from credit list view
    The currency symbols were moved to be handled by the backend or localization system instead of being hardcoded in the view.

--- resources/views/panel/credit/listRefinanciable.blade.php
+++ resources/views/panel/credit/listRefinanciable.blade.php
@@ -100,11 +100,11 @@
             @endif
             <tr>
                 <td>Comisión por apertura:</td>
-                <td>$<span id="content-comision-apertura"></span></td>
+                <td><span id="content-comision-apertura"></span></td>
             </tr>
             <tr>
                 <td>Monto a entregar:</td>
-                <td>$<span id="content-monto-entregar"></span></td>
+                <td><span id="content-monto-entregar"></span></td>
             </tr>
             <tr>
                 <td>Periodicidad:</td>
@@ -116,11 +116,11 @@
             </tr>
             <tr>
                 <td>Pago periódico:</td>
-                <td>$<span id="content-pago-periodico"></span></td>
+                <td><span id="content-pago-periodico"></span></td>
             </tr>
             <tr>
                 <td>Pago total:</td>
-                <td>$<span id="content-pago-total"></span></td>
+                <td><span id="content-pago-total"></span></td>
             </tr>
             <tr>
                 <td>Tasa anual:</td>
